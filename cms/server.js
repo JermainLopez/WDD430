847f7530fb13c8d8c94b153281b9105ccfbd7b23
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2023-03-18 22:09:54 -0600

    W11 Assignment MongoDB database

--- cms/server.js
+++ cms/server.js
@@ -1,72 +1,85 @@
 // Get dependencies
-var express = require('express');
-var path = require('path');
-var http = require('http');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var http = require("http");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-
-
-// import the routing file to handle the default (index) route
-var index = require('./server/routes/app');
 // ROUTES
+const index = require("./server/routes/app");
 const messageRoutes = require("./server/routes/messages");
 const contactRoutes = require("./server/routes/contacts");
 const documentRoutes = require("./server/routes/documents");
-
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
 
 var app = express(); // create an instance of express
 
 // Tell express to use the following parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 app.use(cookieParser());
 
-app.use(logger('dev')); // Tell express to use the Morgan logger
+app.use(logger("dev")); // Tell express to use the Morgan logger
 
 // Add support for CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
     next();
 });
 
 // Tell express to use the specified director as the
 // root directory for your web site
-app.use(express.static(path.join(__dirname, './src')));
+app.use(express.static(path.join(__dirname, "dist/cms")));
 
-// Tell express to map the default route ('/') to the index route
-app.use('/', index);
+//#region "ROUTING"
+// ROUTES
+app.use("/", index);
 app.use("/messages", messageRoutes);
 app.use("/contacts", contactRoutes);
 app.use("/documents", documentRoutes);
 
-// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
-
 // Tell express to map all other non-defined routes back to the index page
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/cms/index.html"));
 });
+//#endregion "ROUTING"
+
+//#region "MONGO"
+// establish a connection to the mongo database
+mongoose.set('strictQuery', false);
+mongoose.connect(
+    process.env.MONGO_URL, {
+        useNewUrlParser: true,
+    },
+    (err, res) => {
+        if (err) console.error("CONNECTION FAILED: " + err);
+        else console.log("Connected to database!");
+    }
+);
+
+//#endregion "MONGO"
 
 // Define the port address and tell express to use this port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
 // Create HTTP server.
 const server = http.createServer(app);
 
 // Tell the server to start listening on the provided port
 server.listen(port, function() {
-    console.log('API running on localhost: ' + port)
+    console.log("API running on localhost: " + port);
 });
--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactsComponent } from './contacts/contacts.component';
@@ -14,7 +15,6 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentItemComponent } from './documents/document-item/document-item.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-import { RouterModule } from '@angular/router';
 import { MessageComponent } from './message/message.component';
 import { MessageListComponent } from './message/message-list/message-list.component';
 import { MessageItemComponent } from './message/message-item/message-item.component';
@@ -22,8 +22,17 @@ import { MessageEditComponent } from './message/message-edit/message-edit.compon
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 import { FormsModule } from '@angular/forms';
+
 import { DndModule } from 'ng2-dnd';
 import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
+
+
+import { DraggableComponent } from 'ng2-dnd';
+import { DraggableHandleComponent } from 'ng2-dnd';
+import { DroppableComponent } from 'ng2-dnd';
+import { SortableComponent } from 'ng2-dnd';
+import { SortableContainer } from 'ng2-dnd';
+import { SortableHandleComponent } from 'ng2-dnd';
 @NgModule({
   declarations: [
     AppComponent,
@@ -46,8 +55,14 @@ import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
     ContactsFilterPipe,
   ],
   imports: [
-    BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, DndModule.forRoot()
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    /* DndModule.forRoot(), */
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
